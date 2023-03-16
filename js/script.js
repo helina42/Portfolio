ef44ef137200d9275777468cbe40853fa8a09495
Helina Alebachew <helinaabebe42@gmail.com>
2023-03-16 10:36:14 +0900

    Add html, javascript and styles to popup after detials-popup

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-/* eslint-disable no-unused-vars */
-// eslint-disable-next-line no-unused-vars
 function popmenu() {
   const mobileMenu = document.querySelector('#popup');
   mobileMenu.classList.toggle('hidden');
